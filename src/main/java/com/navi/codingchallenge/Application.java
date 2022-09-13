7d1cbfa74f48492076a02989b4fa5b103a545916
vish-srivastava <vishal24000@gmail.com>
2022-09-13 19:30:23 +0530

    Minor fix

--- src/main/java/com/navi/codingchallenge/Application.java
+++ src/main/java/com/navi/codingchallenge/Application.java
@@ -12,32 +12,16 @@ public class Application {
         InputCommandHandler inputCommandHandler;
         // file path= C:\Users\Vishal\Desktop\NAVI_INPUT.TXT
         try {
-            System.out.println("Enter File Path or 0 to exit");
+
             String filePath = inputScanner.nextLine();
-            while (!filePath.equalsIgnoreCase("0")) {
-                File file = new File(filePath);
-                inputScanner = new Scanner(file);
-                inputCommandHandler = new InputCommandHandler();
-                while (inputScanner.hasNextLine()) {
-                    String inputLine = inputScanner.nextLine();
-                    inputCommandHandler.handleInput(inputLine);
-                }
-                inputScanner = new Scanner(System.in);
-                System.out.println("Enter File Path or 0 to exit");
-                filePath = inputScanner.nextLine();
+            File file = new File(filePath);
+            inputScanner = new Scanner(file);
+            inputCommandHandler = new InputCommandHandler();
+            while (inputScanner.hasNextLine()) {
+                String inputLine = inputScanner.nextLine();
+                inputCommandHandler.handleInput(inputLine);
             }
             inputScanner.close();
-            if (filePath.equalsIgnoreCase("0")) {
-                System.out.print("Exiting Program in ");
-                Thread.sleep(700);
-                System.out.print("3..");
-                Thread.sleep(700);
-                System.out.print("2..");
-                Thread.sleep(700);
-                System.out.print("1");
-                System.out.println();
-                return;
-            }
 
         } catch (Exception e) {
             e.printStackTrace();
